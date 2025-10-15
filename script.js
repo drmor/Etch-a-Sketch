367139f1f081de69918d196dbd0a708d3f478be1
Miki D <69644623+drmor@users.noreply.github.com.>
2025-10-15 23:22:28 +0500

    refactor code, fixed the div appears, now they appears as a grid, added func so you could paint those divs

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const container = document.querySelector(".container");
-const sizeButton = document.querySelector("#size");
-sizeButton.addEventListener("click", () =>{
-    let num = prompt("choose number")
-    for (let i = 0; i < num; i++) {
-    const grid = document.createElement("div");
-    grid.classList.add("grid");
-    container.appendChild(grid);
-};
+const btnSize = document.getElementById("size");
+const btnClear = document.getElementById("clear");
+const divs = [];
+createGrid(16);
+
+btnSize.addEventListener("click", () => {
+    container.textContent = "";
+    let n = prompt("number");
+    createGrid(n);
+})
+
+btnClear.addEventListener("click", () => {
+    divs.forEach(div => {
+        div.style.backgroundColor = 'white';
+    });
 });
+
+function createGrid(n){
+    for (let i = 0; i < n * n; i++){
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.style.width = container.clientWidth / n + "px";
+        container.appendChild(cell);
+        divs.push(cell)
+    }
+    divs.forEach(div => {
+        div.addEventListener('mousemove', () => {
+            div.style.backgroundColor = 'black';
+        });
+    });
+}
