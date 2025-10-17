de724fdc63a9ec693c92cc345454b0e146823dab
Miki D <69644623+drmor@users.noreply.github.com.>
2025-10-17 23:46:17 +0500

    Did extra funny button, with it you will draw with random colors

--- script.js
+++ script.js
@@ -2,11 +2,30 @@ const container = document.querySelector(".container");
 const btnSize = document.getElementById("size");
 const btnClear = document.getElementById("clear");
 const btnColors = document.querySelectorAll(".colorButton");
+const btnExtra = document.getElementById("randomColor");
 const divs = [];
+const colors = ["red", "orange", "yellow", "green", "blue", "purple", "black"];
 let colorChoice = "black";
 let mouseDown = false;
+
 createGrid(16);
 colorChoose();
+
+function randomColorDraw(){
+    divs.forEach(rgb => {
+        rgb.addEventListener("mousemove", () => {
+            const rng = Math.floor(Math.random() * colors.length);
+            colorChoice = colors[rng];
+            if (mouseDown === true){
+                rgb.style.backgroundColor = colorChoice;
+            };
+        });  
+    });
+};
+
+btnExtra.addEventListener("click", () => {
+    randomColorDraw();
+});
 
 function colorChoose(){
     btnColors.forEach(btn => {
@@ -15,20 +34,15 @@ function colorChoose(){
         });
     });
 };
-
+container.addEventListener("mousedown", () => {
+    mouseDown = true;
+});
+document.addEventListener("mouseup", () => {
+    mouseDown = false;
+});
 function draw(){
-    container.addEventListener("mousedown", () => {
-        mouseDown = true;
-    });
-    document.addEventListener("mouseup", () => {
-        mouseDown = false;
-    });
-    container.addEventListener("click", () => {
-        mouseDown = true;
-        mouseDown = false;
-    });
     divs.forEach(div => {
-        div.addEventListener('mousemove', () => {
+        div.addEventListener("mousemove", () => {
             if (mouseDown === true){
                 div.style.backgroundColor = colorChoice;
             };
@@ -39,26 +53,26 @@ function draw(){
 btnSize.addEventListener("click", () => {
     container.textContent = "";
     createGrid(getNumber());
-})
+});
 
 btnClear.addEventListener("click", () => {
     divs.forEach(div => {
-        div.style.backgroundColor = 'white';
+        div.style.backgroundColor = "white";
     });
 });
 function getNumber(){
     let n = prompt("Insert size number (min - 9, max - 100)", 16);
     if (n === null){
-        n = 16
+        n = 16;
     } else if (n < 9){
         alert("Wrong input, try again")
-        return
+        return;
     } else if (n > 100){
         alert("Wrong input, try again")
-        return
+        return;
     };
     return n;
-}
+};
 function createGrid(n){
     for (let i = 0; i < n * n; i++){
         const cell = document.createElement("div");
@@ -66,6 +80,6 @@ function createGrid(n){
         cell.style.width = container.clientWidth / n + "px";
         container.appendChild(cell);
         divs.push(cell)
-    }
-    draw()
+    };
+    draw();
 };
