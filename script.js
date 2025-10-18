a7bd69d5b611e97ea6d3473296e132cac8afe401
Miki D <69644623+drmor@users.noreply.github.com.>
2025-10-18 22:28:02 +0500

    Cleared the code, fixed bug when you dont inserts any walue with size button > clear button will delete the grid, also now you can draw with only one click

--- script.js
+++ script.js
@@ -12,77 +12,7 @@ let number = 16;
 createGrid(16);
 colorChoose();
 
-function rngColor(e){
-    const rng = Math.floor(Math.random() * colors.length);
-    colorChoice = colors[rng];
-    if (mouseDown === true){
-        e.target.style.backgroundColor = colorChoice;
-    };
-};
-
-function drawRngColor(enable){
-    divs.forEach(div => {
-        if (enable){
-            div.addEventListener("mousemove", rngColor);
-        } else {
-            div.removeEventListener("mousemove", rngColor);
-        }
-    });
-};
-
-btnExtra.addEventListener("click", () => {
-    drawRngColor(true);
-});
-
-function colorChoose(){
-    btnColors.forEach(btn => {
-        btn.addEventListener("click", () => {
-            colorChoice = btn.id;
-        });
-    });
-};
-container.addEventListener("mousedown", () => {
-    mouseDown = true;
-});
-document.addEventListener("mouseup", () => {
-    mouseDown = false;
-});
-function draw(){
-    divs.forEach(div => {
-        div.addEventListener("mousemove", () => {
-            if (mouseDown === true){
-                div.style.backgroundColor = colorChoice;
-            };
-        });
-    });
-};
-
-btnSize.addEventListener("click", () => {
-    drawRngColor(false)
-    colorChoice = "black"
-    container.textContent = "";
-    createGrid(getNumber());
-});
-
-btnClear.addEventListener("click", () => {
-    colorChoice = "black"
-    container.textContent = "";
-    createGrid(number)
-});
-function getNumber(){
-    let n = prompt("Insert size number (min - 9, max - 100)", 16);
-    number = n;
-    if (n === null){
-        n = 16;
-    } else if (n < 9){
-        alert("Wrong input, try again")
-        return;
-    } else if (n > 100){
-        alert("Wrong input, try again")
-        return;
-    };
-    return n;
-};
+//Func that create new grid
 function createGrid(n){
     for (let i = 0; i < n * n; i++){
         const cell = document.createElement("div");
@@ -93,3 +23,103 @@ function createGrid(n){
     };
     draw();
 };
+
+//Func to request a number from a user (with limits)
+function getNumber(){
+    let n = prompt("Insert size number (min - 9, max - 100)", 16);
+    number = n;
+    if (n === null){
+        n = 16;
+        number = 16;
+    } else if (n < 9){
+        alert("Wrong input, try again")
+        return;
+    } else if (n > 100){
+        alert("Wrong input, try again")
+        return;
+    };
+    return n;
+};
+
+// Here is a flag that takes the value true or false when you interact with the mouse.
+container.addEventListener("mousedown", () => {
+    mouseDown = true;
+});
+document.addEventListener("mouseup", () => {
+    mouseDown = false;
+});
+
+// Func that allows you to draw when you click/hold your mouse
+function draw(){
+    divs.forEach(div => {
+        div.addEventListener("mousemove", () => {
+            if (mouseDown === true){
+                div.style.backgroundColor = colorChoice;
+            };
+        });
+    });
+    divs.forEach(div => {
+        div.addEventListener("click", () => {
+            div.style.backgroundColor = colorChoice;
+        });
+    });
+};
+
+//Btn event, which calls the function getNumber() to request a number for the grid size and calls the function createGrid() to create a grid with this value
+btnSize.addEventListener("click", () => {
+    colorChoice = "black"
+    container.textContent = "";
+    createGrid(getNumber());
+});
+
+//Btn event that clears the grid by creating a completely new one with the old value selected by the user or the default value
+btnClear.addEventListener("click", () => {
+    colorChoice = "black"
+    container.textContent = "";
+    createGrid(number);
+});
+
+//Btn event that allows the user to select a color, the color value is taken from the color button ID.
+function colorChoose(){
+    btnColors.forEach(btn => {
+        btn.addEventListener("click", () => {
+            colorChoice = btn.id;
+        });
+    });
+};
+
+// Extra task, btn that make you crazy and give you power to draw with random colors!!! 
+btnExtra.addEventListener("click", () => {
+    drawRngColor(true);
+});
+
+//Func that appeals to array "colors" and take random value from it
+function rngColor(e){
+    const rng = Math.floor(Math.random() * colors.length);
+    colorChoice = colors[rng];
+    if (mouseDown === true){
+        e.target.style.backgroundColor = colorChoice;
+    };
+};
+
+//Func that allow you to draw with random colors
+function drawRngColor(enable){
+    divs.forEach(div => {
+        if (enable){
+            div.addEventListener("mousemove", rngColor);
+        } else {
+            div.removeEventListener("mousemove", rngColor);
+        }
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
